892665688c62abbf26dde28ae094363dc8550588
shigemk2 <i.am.shige@gmail.com>
2015-03-30 21:33:10 +0900

    列挙型

--- enumeration.scala
+++ enumeration.scala
@@ -3,3 +3,19 @@ case class Blue() extends Color
 case class Red() extends Color
 case class Green() extends Color
 case class White() extends Color
+
+object Enumeration {
+  def f(color: Color) = {
+    color match {
+      case r: Red => r
+      case b: Blue => b
+      case g: Green => g
+    }
+  }
+
+  def main(args: Array[String]): Unit = {
+    val blue = new Blue()
+    println(f(blue))
+  }
+}
+
