b622ee92c0da9835b720af06b812eedfa66ddfc6
shigemk2 <i.am.shige@gmail.com>
2015-03-31 22:50:38 +0900

    列挙型 いろいろなメソッド

--- enum.scala
+++ enum.scala
@@ -6,13 +6,21 @@ object Main {
   import Color._
 
   def main(args: Array[String]): Unit = {
+    // id
     println(Blue.id)
     println(Red.id)
     println(Green.id)
     println(White.id)
+    // apply
     println(Color(0))
     println(Color(1))
     println(Color(2))
     println(Color(3))
+    // maxId
+    println(Color.maxId)
+    // values
+    println(Color.values)
+    // withName
+    println(Color.withName("Red"))
   }
 }
