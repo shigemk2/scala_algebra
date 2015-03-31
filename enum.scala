973fcfb801a8e5c39d6db3a529e01178603e9dea
shigemk2 <i.am.shige@gmail.com>
2015-03-31 22:16:10 +0900

    列挙型 id

--- enum.scala
+++ enum.scala
@@ -1,24 +1,18 @@
-sealed abstract class Color(n: Int)
-case class Blue() extends Color(0)
-case class Red() extends Color(1)
-case class Green() extends Color(2)
-case class White() extends Color(3)
-
-object Enum {
-  def f(color: Color) = {
-    color match {
-      case r: Red => r
-      case b: Blue => b
-      case g: Green => g
-      case w: White => w
-    }
-  }
-
-  def main(args: Array[String]): Unit = {
-    println(f(new Red()))
-    println(f(new Blue()))
-    println(f(new Green()))
-    println(f(new White()))
-  }
+object Color extends Enumeration {
+  val Blue, Red, Green, White = Value
 }
 
+object Main {
+  import Color._
+
+  def main(args: Array[String]): Unit = {
+    println(Blue.id)
+    println(Red.id)
+    println(Green.id)
+    println(White.id)
+    println(Color(0))
+    println(Color(1))
+    println(Color(2))
+    println(Color(3))
+  }
+}
