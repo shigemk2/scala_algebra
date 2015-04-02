d34cf4ec9eef3e0c44749ea602414cae56189429
shigemk2 <i.am.shige@gmail.com>
2015-04-03 08:37:43 +0900

    mix ver1

--- mix.scala
+++ mix.scala
@@ -5,22 +5,14 @@ object Color extends Enumeration {
 object Main {
   import Color._
 
+  def mix(a:Color.Value, b:Color.Value): Color.Value = {
+    Color(a.id + b.id)
+  }
+
   def main(args: Array[String]): Unit = {
-    // id
-    println(Blue.id)
-    println(Red.id)
-    println(Green.id)
-    println(White.id)
-    // apply
-    println(Color(0))
-    println(Color(1))
-    println(Color(2))
-    println(Color(3))
-    // maxId
-    println(Color.maxId)
-    // values
-    println(Color.values)
-    // withName
-    println(Color.withName("Red"))
+    // mix
+    println(mix(Blue, Blue))
+    // mix
+    println(mix(Blue, Red))
   }
 }
