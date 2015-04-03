308b488ab251eb22ef25595cc4830a93312dd5f1
shigemk2 <i.am.shige@gmail.com>
2015-04-03 23:26:08 +0900

    【問1】光の三原色と、2つの色を混合する関数mixを定義してください。混ぜることによってできる色も定義の対象とします。ただし同じ成分同士は強め合わないものとします。

--- mix.scala
+++ mix.scala
@@ -6,13 +6,31 @@ object Main {
   import Color._
 
   def mix(a:Color.Value, b:Color.Value): Color.Value = {
-    Color(a.id + b.id)
+    (a, b) match {
+      case (Blue, Red)       => Magenta
+      case (Blue, Magenta)   => Magenta
+      case (Blue, Green)     => Cyan
+      case (Blue, Cyan)      => Cyan
+      case (Blue, Yellow)    => White
+      case (Red, Magenta)    => Magenta
+      case (Red, Green)      => Yellow
+      case (Red, Cyan)       => White
+      case (Red, Yellow)     => Yellow
+      case (Magenta, Green)  => White
+      case (Magenta, Cyan)   => White
+      case (Magenta, Yellow) => White
+      case (Green, Cyan)     => Cyan
+      case (Green, Yellow)   => Yellow
+      case (Cyan, Yellow)    => White
+      case (White, _)        => White
+      case (a, b) if a == b  => a
+      case (a, b)            => mix(b, a)
+    }
   }
 
   def main(args: Array[String]): Unit = {
-    // mix
-    println(mix(Blue, Blue))
-    // mix
-    println(mix(Blue, Red))
+    println(mix(Red, Red))
+    println(mix(Red, Blue))
+    println(mix(Blue, mix(Blue, Green)))
   }
 }
