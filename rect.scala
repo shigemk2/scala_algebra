75105a1dc9ea33f3f7705901d57d759d1d7354c0
shigemk2 <i.am.shige@gmail.com>
2015-04-06 22:47:20 +0900

    【問2】x,y,w,hを表現したRect型を定義して、RectにPointが含まれるかどうかを判定する関数containsを実装してください。

---
+++ rect.scala
@@ -0,0 +1,6 @@
+println(contains(new Rect(2, 2, 3, 3), new Point(1, 1)))
+println(contains(new Rect(2, 2, 3, 3), new Point(2, 2)))
+println(contains(new Rect(2, 2, 3, 3), new Point(3, 3)))
+println(contains(new Rect(2, 2, 3, 3), new Point(4, 4)))
+println(contains(new Rect(2, 2, 3, 3), new Point(5, 5)))
+
