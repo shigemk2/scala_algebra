634764786d711e1aa3b7b83bc11d048e673d3000
shigemk2 <i.am.shige@gmail.com>
2015-04-26 21:47:06 +0900

    無限ループ対策

--- mix.hs
+++ mix.hs
@@ -1,25 +1,27 @@
 data Color = Blue | Red | Magenta | Green | Cyan | Yellow | White
-    deriving Show
+    deriving (Show, Eq)
 
-mix Blue    Red     = Magenta
-mix Blue    Magenta = Magenta
-mix Blue    Green   = Cyan
-mix Blue    Cyan    = Cyan
-mix Blue    Yellow  = White
-mix Red     Magenta = Magenta
-mix Red     Green   = Yellow
-mix Red     Cyan    = White
-mix Red     Yellow  = Yellow
-mix Magenta Green   = White
-mix Magenta Cyan    = White
-mix Magenta Yellow  = White
-mix Green   Cyan    = Cyan
-mix Green   Yellow  = Yellow
-mix Cyan    Yellow  = White
-mix White   _       = White
-mix c1      c2      = mix c2 c1
+mix Blue    Red       = Magenta
+mix Blue    Magenta   = Magenta
+mix Blue    Green     = Cyan
+mix Blue    Cyan      = Cyan
+mix Blue    Yellow    = White
+mix Red     Magenta   = Magenta
+mix Red     Green     = Yellow
+mix Red     Cyan      = White
+mix Red     Yellow    = Yellow
+mix Magenta Green     = White
+mix Magenta Cyan      = White
+mix Magenta Yellow    = White
+mix Green   Cyan      = Cyan
+mix Green   Yellow    = Yellow
+mix Cyan    Yellow    = White
+mix White   _         = White
+mix c1 c2 | c1 == c2  = c1
+          | otherwise = mix c2 c1
 
 main = do
+    print $ mix Blue Blue
     print $ mix Red Blue
     print $ mix Red $ mix Blue Green
     print $ mix Red Red
